ac82d38d8efe451d6548d4f155dd0f32f8d2646a
Vahlaz <raty.atte@gmail.com>
2020-09-02 21:23:04 +0300

    osa3

---
+++ build/precache-manifest.737b69064e7b9e82fb9eef980ad059fe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "096d5348e27b23f70d0f1c240eb9678b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6afbedb3b9a54222244",
+    "url": "/static/css/main.404de3af.chunk.css"
+  },
+  {
+    "revision": "d72cc7c3f59543947486",
+    "url": "/static/js/2.029bf66b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.029bf66b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b6afbedb3b9a54222244",
+    "url": "/static/js/main.f6e7abca.chunk.js"
+  },
+  {
+    "revision": "f0be2d6b237b3fb64219",
+    "url": "/static/js/runtime-main.24b8e7b7.js"
+  }
+]);
